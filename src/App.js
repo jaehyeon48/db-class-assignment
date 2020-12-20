8171b9622bd097f69b6c9af93ca22fa31f1df9ad
jaehyeon48 <imjaehyeon48@gmail.com>
2020-12-20 17:50:53 +0900

    initial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
+import FileInput from './components/FileInput';
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="main-page">
+      <h1 className="main-header">201629115 김재현 데이터베이스 과제 사이트</h1>
+      <FileInput />
+    </main>
   );
 }
 
---
+++ src/components/FileInput.js
@@ -0,0 +1,117 @@
+import React, { useState, useRef } from 'react';
+import axios from 'axios';
+
+const FileInput = () => {
+  const fileInputRef = useRef(null);
+  const [applyDragStyle, setApplyDragStyle] = useState(false);
+  const [isFileLoaded, setIsFileLoaded] = useState(false);
+
+  const handleFileLoadedStatus = (e) => {
+    if (fileInputRef.current.files.length > 0) {
+      setIsFileLoaded(true);
+      setApplyDragStyle(false);
+    }
+    else {
+      setIsFileLoaded(false);
+    }
+  }
+
+  const handleFileInput = () => {
+    const reader = new FileReader();
+    reader.onload = async () => {
+      const inputDataArray = reader.result.split('\n').map((line) => splitData(line));
+      inputDataArray.sort((a, b) => {
+        if (a[0] > b[0]) return 1;
+        else if (a[0] < b[0]) return -1;
+        return 0;
+      });
+      // const postBody = JSON.stringify({ inputData: inputDataArray });
+      // const config = {
+      //   headers: {
+      //     'Content-Type': 'application/json'
+      //   }
+      // };
+      // try {
+      //   await axios.post('http://localhost:5000/api/insert/by-file', postBody, config);
+      // } catch (error) {
+      //   console.log(error);
+      // }
+    }
+    reader.readAsText(fileInputRef.current.files[0]);
+  }
+
+  const handleDragEnter = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    setApplyDragStyle(true);
+  }
+
+  const handleDragLeave = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    setApplyDragStyle(false);
+  }
+
+  const handleDrag = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    setApplyDragStyle(true);
+  }
+
+  const handleDrop = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    fileInputRef.current.files = e.dataTransfer.files;
+    handleFileLoadedStatus();
+  }
+
+  const handleOpenFileSelect = () => {
+    fileInputRef.current.click();
+  }
+
+  return (
+    <React.Fragment>
+      <p className="file-input-notice">파일로 데이터 입력하기</p>
+      <div
+        className="file-drop-zone"
+        style={applyDragStyle ? { backgroundColor: '#60768e', boxShadow: '5px 5px 23px #000' } : null}
+        // onDragEnter={handleDragEnter}
+        onDragLeave={handleDragLeave}
+        onDragOver={handleDrag}
+        onDrop={handleDrop}
+      >
+        <input
+          className="file-input"
+          type="file"
+          accept=".csv"
+          ref={fileInputRef}
+          onChange={handleFileLoadedStatus}
+        />
+        <button
+          type="button"
+          className="choose-file-btn"
+          onClick={handleOpenFileSelect}
+        >파일선택</button>
+        {isFileLoaded ? <p>현재 파일: {fileInputRef.current.files[0].name}</p> : <p>파일을 여기다 끌어 놓아주세요.</p>}
+      </div>
+    </React.Fragment>
+  );
+}
+
+function splitData(line) {
+  line += ',';
+  let splitResult = [];
+  let processedString = '';
+  for (const char of line) {
+    if (char !== ',') {
+      processedString += char;
+    }
+    else if (char === ',' && processedString.trim().length > 0) {
+      splitResult.push(processedString.trim());
+      processedString = '';
+    }
+  }
+  return splitResult;
+}
+
+export default FileInput;

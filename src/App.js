b4fb0f9f04863f48a683105cdfd9d089b9b010d8
jaehyeon48 <imjaehyeon48@gmail.com>
2020-12-21 10:12:21 +0900

    modify page structure and style

--- src/App.js
+++ src/App.js
@@ -6,11 +6,11 @@ import FileInput from './components/FileInput';
 
 const App = () => {
   return (
-    <main className="main-page">
+    <section className="input-page">
       <h1 className="main-header">201629115 김재현 데이터베이스 과제 사이트</h1>
       <GuiInput />
       <FileInput />
-    </main>
+    </section>
   );
 }
 
